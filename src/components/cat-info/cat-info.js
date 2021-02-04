ebe58e5c47282349f63a46b3a095883cf2c44d14
jaylordibe <jaylordibe@gmail.com>
2021-02-04 13:37:53 +0800

    Feature: Created cat info page

---
+++ src/components/cat-info/cat-info.js
@@ -0,0 +1,51 @@
+import {useParams} from 'react-router-dom';
+import {Button, Card, Col, Row, Spinner} from 'react-bootstrap';
+import React, {useEffect, useState} from 'react';
+import CatService from '../../services/cat.service';
+
+export default function CatInfo() {
+
+    const params = useParams();
+    const catId = params ? params.catId : '';
+    const [catInfo, setCatInfo] = useState({});
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        CatService.getCatInfoById(catId).then((response) => {
+            if (response && response.breeds && response.breeds.length) {
+                setCatInfo(response);
+                setLoading(false);
+            }
+        });
+    }, [catId]);
+
+    return (
+        <React.Fragment>
+            <Row className="justify-content-center">
+                {
+                    loading ?
+                        <Col sm={12} className="text-center">
+                            <Spinner animation="grow"/>
+                        </Col>
+                        :
+                        <Col sm={6}>
+                            <Card>
+                                <Card.Img variant="top" src={catInfo.url} className="img-fluid"/>
+                                <Card.Body>
+                                    <Card.Title>{catInfo.breeds[0].name.toUpperCase()}</Card.Title>
+                                    <Card.Text>
+                                        Origin: {catInfo.breeds[0].origin}
+                                    </Card.Text>
+                                    <Card.Text>
+                                        Description: {catInfo.breeds[0].description}
+                                    </Card.Text>
+                                    <Button href={`/?breed=${catInfo.breeds[0].id}`} variant="primary" block>Go
+                                        Back</Button>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                }
+            </Row>
+        </React.Fragment>
+    );
+}
